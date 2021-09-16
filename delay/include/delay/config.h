e7cd1c9ace1f52cf04b51cf05637e84b702e6a01
Galaxy1stShooter <79631580+Galaxy1stShooter@users.noreply.github.com>
2021-09-16 15:24:10 +0800

    Add files via upload

---
+++ delay/include/delay/config.h
@@ -0,0 +1,23 @@
+//server listen addr
+char server_addr_list[5][30]={
+    "tcp://*:5555",
+    "tcp://*:6666",
+    "tcp://*:5050",
+    "tcp://*:6060",
+    "tcp://*:6565"
+};
+//client connect addr
+//replace the localhost to the actual server addr
+//keep the port coinstency with that in server_addr_list
+char connect_addr_list[5][30]={
+    "tcp://localhost:5555",
+    "tcp://192.168.1.144:6666",
+    "tcp://192.168.1.144:5050",
+    "tcp://192.168.1.144:6060",
+    "tcp://192.168.1.144:6565"
+};
+//client subscribe addr
+//replace the localhost to the actual server addr
+char subaddr[30]="tcp://localhost:7050";
+//server publish addr
+char pubaddr[30]="tcp://*:7050";
